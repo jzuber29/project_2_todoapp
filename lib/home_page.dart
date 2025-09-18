5abc87d5f5b1cf1dec33233d2db3fba7a08c6a42
jzuber29 <jzuber29@gmail.com>
2025-09-18 10:54:00 +0530

    Initial commit: Flutter Todo App

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      appBar: AppBar(
+        title: Text('TODO'),
+        backgroundColor: Colors.deepPurple[400],
+        elevation: 10,
+      ),
+    )
